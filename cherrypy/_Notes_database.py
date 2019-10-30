49be94f18e5e64da577dc81b66e4436079846752
MJMalir <mmalir@nd.edu>
2019-10-29 23:50:31 -0400

    updates to notes db & controller

--- cherrypy/_Notes_database.py
+++ cherrypy/_Notes_database.py
@@ -1,27 +1,40 @@
+# Notes DB -- version 1.1
+# Matthew J. Malir
+
 import os
 import requests
 import json
+import csv
+import pickle
 from datetime import datetime
 
 
 class _Notes_database:
-
-	# initialize databse
+	# initialize database
 	def __init__(self):
-		self.notes = dict()
-		# self.outfile = open("note_output.txt","w+")
+		try: # try to load existing db
+			in_file = open("data/note_db.pickle","rb")
+			self.notes = pickle.load(in_file)
+		except: # create new db
+			self.notes = dict()
 
 	def add_note(self, data):
 		now = datetime.now()
 		timestamp = now.strftime("%d/%m %H:%M:%S")
 
-		note = data["note"]
+		note = data["note"][1:]  # strip leading newline
 		self.notes[timestamp] = note
-		# DEBUG:
-		print(self.notes)
 
-		# self.outfile.write("timestamp: {}\n".format(timestamp))
-		# self.outfile.write("note: \n{}\n\n".format(note))
+		# update physical db
+		self.write_db()
+
+	def set_note(self,timestamp,new_note):
+		if timestamp in self.notes.keys():
+			self.notes[timestamp] = new_note
+
+		# update physical db
+		self.write_db()
+
 
 	def get_note(self, timestamp):
 		if timestamp in self.notes.keys():
@@ -29,12 +42,30 @@ class _Notes_database:
 		else:
 			return "Not Found."
 
-	def get_all_notes(self):
-		return json.dumps(self.notes)
+	# def get_all_notes(self):
+	# 	return json.dumps(self.notes)
 
 	def delete_note(self, timestamp):
 		self.notes.pop(timestamp)
 
-	def delete_all_notes(self):
-		self.notes.clear()
+		# update physical db
+		self.write_db()
+
+	# def delete_all_notes(self):
+	# 	self.notes.clear()
+	#
+	# 	# update physical db
+	# 	self.write_db()
+
+	# write changes to physical db
+	def write_db(self):
+		# write to csv for viewing
+		w = csv.writer(open("data/note_db_current.csv", "w"))
+		for key, val in self.notes.items():
+		    w.writerow([key, val])
+		# write to pickle file for restarting db
+		out_file = open("data/note_db.pickle","wb")
+		pickle.dump(self.notes,out_file)
+		out_file.close()
+
 
--- cherrypy/notes.py
+++ cherrypy/notes.py
@@ -2,26 +2,25 @@ import json
 import cherrypy
 
 class notesController:
-
 	def __init__(self, notesdb):
 		self.notesdb = notesdb
-	
+
 	# create a new note
 	def POST_NOTE(self):
 		msg = json.loads(cherrypy.request.body.read())
 		self.notesdb.add_note(msg)
 		return json.dumps({"result": "success"})
-	
+
 	# get an existing note
 	def GET_NOTE(self, note_id):
-		return json.dumps(self.notesdb.get_note(int(note_id)))
+		return json.dumps(self.notesdb.get_note(note_id))
 
 	# change an existing note
 	def PUT_NOTE(self, note_id):
-		self.notesdb.set_note(int(note_id), json.loads(cherrpy.request.body.read()))
+		self.notesdb.set_note(note_id, json.loads(cherrpy.request.body.read()))
 		return json.dumps({"result": "success"})
 
 	# delete an existing note
 	def DELETE_NOTE(self, note_id):
-		self.notesdb.delete_note(int(note_id))
+		self.notesdb.delete_note(note_id)
 		return json.dumps({"result": "success"})
