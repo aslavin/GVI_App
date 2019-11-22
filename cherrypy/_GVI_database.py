66efaadb7cbf0bbd4c0e8711370ea2851e4d8507
areardon1928 <44825818+areardon1928@users.noreply.github.com>
2019-11-21 19:57:26 -0500

    Uncommented Sharepoint Objects

--- cherrypy/_GVI_database.py
+++ cherrypy/_GVI_database.py
@@ -17,12 +17,12 @@ class _GVI_database:
 		user = os.getenv("GVI_USER")
 		passwd = os.getenv("GVI_PASSWD")
 		url = 'https://cseserviceproj.sharepoint.com'
-		'''
+	
 		self.spSite = testShare.initSite(user, passwd, url)
 		self.spList = testShare.getList(self.spSite, "TestList_Collection")
 		self.contacts = {}
 		self.contacts[0] = {"key": "value"}
-		'''
+	
 	
 
 	# set a new contact
